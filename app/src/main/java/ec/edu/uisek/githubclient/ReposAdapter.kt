e5bed804346b618f517dab611b03df750ae31678
nelson guijarro <nrguijarro@puce.edu.ec>
2025-11-11 16:33:34 -0500

    Lab 4 parte 1

--- app/src/main/java/ec/edu/uisek/githubclient/ReposAdapter.kt
+++ app/src/main/java/ec/edu/uisek/githubclient/ReposAdapter.kt
@@ -3,27 +3,32 @@ package ec.edu.uisek.githubclient
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import ec.edu.uisek.githubclient.databinding.FragmentRepoItemBinding
+import ec.edu.uisek.githubclient.models.Repo
 
 // 1. Clase ViewHolder: Contiene las referencias a las vistas de un solo ítem.
 //    Usa la clase de ViewBinding generada para fragment_repo_item.xml.
 class RepoViewHolder(private val binding: FragmentRepoItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
     // 2. Función para vincular datos a las vistas del ítem.
-    fun bind(position: Int) {
-    //    Por ahora, usaremos datos de ejemplo.
-        binding.repoOwnerImage.setImageResource(R.mipmap.ic_launcher) // Usa una imagen que tengas
-        binding.repoName.text = "Repositorio #${position + 1}"
-        binding.repoDescription.text = "Esta es la descripción del elemento número ${position + 1} en la lista."
-        binding.repoLanguage.text = if (position % 2 == 0) "Kotlin" else "Java"
+    fun bind(repo: Repo) {
+        binding.repoName.text = repo.name
+        binding.repoDescripcion.text = repo.description ?: "EL REPOSITORIO NO TIENE DESCRIPCIÓN"
+        binding.repoLang.text = repo.language ?: "LENGUAJE NO ESPECIFICADO"
+        Glide.with(binding.root.context)
+            .load(repo.owner.avatarUrl)
+            .placeholder(R.mipmap.ic_launcher)
+            .error(R.mipmap.ic_launcher)
+            .circleCrop()
+            .into(binding.repoOwnerImage)
     }
 }
 
 // 3. Clase Adapter: Gestiona la creación y actualización de los ViewHolders.
 class ReposAdapter : RecyclerView.Adapter<RepoViewHolder>() {
-
-    // Por ahora, simplemente le diremos que muestre 3 ítems.
-    override fun getItemCount(): Int = 3
+    private  var repositories : List<Repo> = emptyList()
+    override fun getItemCount(): Int = repositories.size
 
     // Se llama para crear un nuevo ViewHolder cuando el RecyclerView lo necesita.
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepoViewHolder {
@@ -38,6 +43,11 @@ class ReposAdapter : RecyclerView.Adapter<RepoViewHolder>() {
 
     // Se llama para vincular los datos a un ViewHolder en una posición específica.
     override fun onBindViewHolder(holder: RepoViewHolder, position: Int) {
-        holder.bind(position)
+        holder.bind(repositories[position])
+    }
+
+    fun updateRepositories(newRepos: List<Repo>) {
+        repositories = newRepos
+        notifyDataSetChanged()
     }
 }
